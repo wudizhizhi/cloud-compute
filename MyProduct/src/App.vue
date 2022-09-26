d08736b5ff9368a11a6b262fffadf2e7f4c134d1
wudizhizhi <3087591062@qq.com>
2022-09-26 16:24:41 +0800

    xxx

---
+++ MyProduct/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <router-view/>
+
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html,body,#app{
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+
+</style>
